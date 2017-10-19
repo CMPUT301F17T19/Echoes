88ec75fd121a2dd85d3f84c866cb555c53453963
AmyShanLu <slu1@ualberta.ca>
2017-10-19 11:40:34 -0600

    Unit test class for HabitEventList

---
+++ Echoes/app/src/androidTest/java/com/example/cmput301f17t19/echoes/HabitEventListTest.java
@@ -0,0 +1,40 @@
+/*
+ * Class Name: HabitEventListTest
+ *
+ * Version: Version 1.0
+ *
+ * Date: October 19th, 2017
+ *
+ * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+ */
+
+package com.example.cmput301f17t19.echoes;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Unit Test for HabitEvent class
+ *
+ * @author Shan Lu
+ * @version 1.0
+ * @since 1.0
+ */
+public class HabitEventListTest extends ActivityInstrumentationTestCase2 {
+    public HabitEventListTest(){
+        super(com.example.cmput301f17t19.echoes.HabitHistoryActivity.class);
+    }
+
+    /**
+     * Test for add(HabitEvent habitEvent) in HabitEventList class
+     */
+    public void testAdd(){
+        //TODO: testAdd method after HabitEvent class is done
+    }
+
+    /**
+     * Test for remove(int idx) in HabitEventList class
+     */
+    public void testRemove(){
+        //TODO: testRemove method after HabitEvent class is done
+    }
+}
