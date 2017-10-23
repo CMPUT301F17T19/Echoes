f7f4a0a0fbaa1b2fc721fea6cb6811ea0fa4ed37
zhaozhen@ualberta.ca <zhaozhen@ualberta.ca>
2017-10-23 12:03:16 -0600

    update

--- Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/UserProfile.java
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/UserProfile.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 
 public class UserProfile {
 
+    protected ArrayList<Request> requests;
+    private String id;
     public String username;  // unique user
     public String email_address;     //user contact email address
     public String phone_number;
@@ -22,21 +24,32 @@ public class UserProfile {
     public ArrayList<following>following_list;
     public ArrayList<follower>follower_list;
 
-    public UserProfile(String username, String email_address, String phone_number, String comment, Image profile_picture, ArrayList<following>following_list, ArrayList<follower>follower_list) {
+    public UserProfile(String username, String email_address, String phone_number, String comment, Image profile_picture) {
         this.username = username;
         this.email_address = email_address;
         this.phone_number = phone_number;
         this.comment = comment;
         this.profile_picture = profile_picture;
-        this.following_list = following_list;
-        this.follower_list = follower_list;
 
+        this.following_list = new ArrayList<>();
+        this.follower_list = new ArrayList<>();
+        this.requests = new ArrayList<>();
+
+    }
+
+    //generate unique id using hashing function according to the username string
+    public void setID(String id){
+        this.id = id;
+    }
+
+
+    public void addRequest(Request request){
+        requests.add(request);
     }
 
     public String getUserName(){
         return username;
     }
-
 
     public String getEmailAddress(){
         return email_address;
