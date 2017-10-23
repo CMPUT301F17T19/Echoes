190beb5514a132d9c73a6a2d3fdab5f934e4e8cf
AmyShanLu <slu1@ualberta.ca>
2017-10-23 13:35:10 -0600

    Unit test for HabitList class

--- Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/HabitList.java
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/HabitList.java
@@ -1,49 +1,70 @@
-///*
-// * Class Name: HabitEventList
-// *
-// * Version: Version 1.0
-// *
-// * Date: October 20th, 2017
-// *
-// * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
-// */
-//
-//package com.example.cmput301f17t19.echoes;
-//
-//import java.util.ArrayList;
-//
-///**
-// * Represents a List of User's habits
-// *
-// * @author Shan Lu
-// * @version 1.0
-// * @since 1.0
-// */
-//public class HabitList {
-//    private ArrayList<Habit> habits = new ArrayList<Habit>();
-//
-//    /**
-//     * Constructs a HabitList object
-//     */
-//    public HabitList(){
-//
-//    }
-//
-//    /**
-//     * Add the Habit object to the HabitList
-//     *
-//     * @param habitEvent: HabitEvent object
-//     */
-//    public void add(Habit habit){
-//        this.habits.add(habit);
-//    }
-//
-//    /**
-//     * Remove the Habit object at the index idx from the HabitList
-//     *
-//     * @param idx: integer
-//     */
-//    public void remove(int idx){
-//        this.habits.remove(idx);
-//    }
-//}
+/*
+ * Class Name: HabitEventList
+ *
+ * Version: Version 1.0
+ *
+ * Date: October 20th, 2017
+ *
+ * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+ */
+
+package com.example.cmput301f17t19.echoes;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a List of User's habits
+ *
+ * @author Shan Lu
+ * @version 1.0
+ * @since 1.0
+ */
+public class HabitList {
+    private ArrayList<Habit> habits = new ArrayList<Habit>();
+
+    /**
+     * Constructs a HabitList object
+     */
+    public HabitList(){
+
+    }
+
+    /**
+     * Add the Habit object to the HabitList
+     *
+     * @param habit: Habit object
+     */
+    public void add(Habit habit){
+        this.habits.add(habit);
+    }
+
+    /**
+     * Remove the Habit object at the index idx from the HabitList
+     *
+     * @param idx: integer
+     */
+    public void remove(int idx){
+        this.habits.remove(idx);
+    }
+
+    /**
+     * Check if the HabitList contains the Habit object
+     *
+     * @param habit: Habit
+     * @return true: if the HabitList contain the input Habit object
+     *         false: if the HabitList does not contain the input Habit object
+     */
+    public boolean hasHabitEvent(Habit habit){
+        return this.habits.contains(habit);
+    }
+
+    /**
+     * Get the habit arraylist
+     *
+     * @return habits: ArrayList<Habit>
+     */
+    public ArrayList<Habit> getHabits(){
+        return this.habits;
+    }
+
+}
---
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/MyHabitsActivity.java
@@ -0,0 +1,23 @@
+/*
+ * Class Name: MyHabitsActivity
+ *
+ * Version: Version 1.0
+ *
+ * Date: October 23rd, 2017
+ *
+ * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+ */
+
+package com.example.cmput301f17t19.echoes;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * My Habits UI
+ *
+ * @author Shan Lu
+ * @version 1.0
+ * @since 1.0
+ */
+public class MyHabitsActivity extends AppCompatActivity {
+}
