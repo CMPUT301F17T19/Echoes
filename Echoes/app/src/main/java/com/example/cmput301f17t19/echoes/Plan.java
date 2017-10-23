ef37f32e2c215115a9a0d4f653a548776c83d0d5
Hayden Bauder <bauder@ualberta.ca>
2017-10-23 13:03:10 -0600

    added Habit and Plan classes

---
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/Plan.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+ */
+
+package com.example.cmput301f17t19.echoes;
+
+/**
+ * Created by Gary's on 23/10/2017.
+ */
+
+public class Plan {
+}
