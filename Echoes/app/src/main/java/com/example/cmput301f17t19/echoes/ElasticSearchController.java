7b72293f3e961c1c6c99ca074bb68d53068adbd0
AmyShanLu <slu1@ualberta.ca>
2017-10-29 22:23:19 -0600

    fix bug

---
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/ElasticSearchController.java
@@ -0,0 +1,26 @@
+///*
+// * Class Name: ElasticSearchController
+// *
+// * Version: Version 1.0
+// *
+// * Date: October 29th, 2017
+// *
+// * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+// */
+//
+//package com.example.cmput301f17t19.echoes;
+//
+//import com.searchly.jestdroid.JestDroidClient;
+//
+///**
+// * Elastic Search Controller
+// *
+// * @author Shan Lu
+// * @version 1.0
+// * @since 1.0
+// */
+//public class ElasticSearchController {
+//    private static JestDroidClient client;
+//
+//
+//}
