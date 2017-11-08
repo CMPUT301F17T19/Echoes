ce2fb9ed95eb0eb4c049b70478e9beb2cdde2de1
slark <610158299@qq.com>
2017-11-08 12:26:31 -0700

    ...

--- Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/main_menu.java
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/main_menu.java
@@ -4,8 +4,10 @@
 
 package com.example.cmput301f17t19.echoes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class main_menu extends AppCompatActivity {
 
@@ -14,4 +16,37 @@ public class main_menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+
+    public void viewMyHabits(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewMyHabits(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewMyHabits(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewMyHabits(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewMyHabits(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
+    }
+
+    public void Logout(View view) {
+        Intent intent = new Intent(this, AddNewActivity.class);
+        startActivity(intent);
+    }
+
+
 }
