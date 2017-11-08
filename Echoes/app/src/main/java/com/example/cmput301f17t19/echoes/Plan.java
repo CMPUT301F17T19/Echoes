2166d525a72d621748c4f6e2e87ae7c12656bf9b
htbauder <bauder@ualberta.ca>
2017-11-08 15:50:58 -0700

    Update Plan.java
    Update. Need to add DailySchedule class

--- Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/Plan.java
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/Plan.java
@@ -4,9 +4,33 @@
 
 package com.example.cmput301f17t19.echoes;
 
+include java.util.Date;
+include java.time.LocalTime;
+
 /**
  * Created by Hayden Bauder on 23/10/2017.
  */
 
 public class Plan {
+    public enum Day {
+        SUNDAY,   //0
+        MONDAY,   //1
+        TUESDAY,  //2
+        WEDNESDAY,//3
+        THURSDAY, //4
+        FRIDAY,   //5
+        SATURDAY, //6
+        NUM_DAYS
+    }
+  
+    public ArrayList<DailySchedule> plan;  //schedule[SUNDAY] should be all scheduled times for sunday   
+  
+    public getToDo(Day) throws InvalidDayException{
+        if (DAY >= NUM_DAYS) {
+            throw new InvalidDayException();
+        }
+        else {
+            return plan.getIndex(DAY);
+        }
+    }  
 }
