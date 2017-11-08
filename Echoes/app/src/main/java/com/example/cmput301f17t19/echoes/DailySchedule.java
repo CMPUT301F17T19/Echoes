a8fc0841b242ef19d5aa649ecb9211617c8a6500
htbauder <bauder@ualberta.ca>
2017-11-08 15:56:44 -0700

    Create DailySchedule.java
    Schedule for Plan object

---
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/DailySchedule.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+ */
+
+package com.example.cmput301f17t19.echoes;
+
+include java.util.Date;
+include java.time.LocalTime;
+
+/**
+ * Created by Hayden Bauder on 7/11/2017.
+ */
+ 
+ // An arraylist of time objects. used by Plan to correspond to a schedule
+public class DailySchedule {
+    public ArrayList<LocalTime> schedule;
+    
+    public DailySchedule(ArrayList<LocalTime> schedule) {
+        this.schedule = schedule;
+    }
+    
+    public DailySchedule() {
+        this.schedule = new ArrayList<LocalTime>;
+    }
+}
