6b5ae6e4f33600bd94a6b2dcb78d44abf2ea5f44
zhaozhen@ualberta.ca <zhaozhen@ualberta.ca>
2017-11-08 21:14:44 -0700

    partial login update

--- Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/LoginActivity.java
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/LoginActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -103,6 +104,43 @@ public class LoginActivity extends AppCompatActivity  {
 
     //handling login
     private void login() {
+
+        // check if username/password are empty
+        int textlength = userEditText.getText().length();
+
+        if (textlength == 0) {
+            Toast.makeText(LoginActivity.this, "Please provide a Username!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //check if username exists in the server
+        ElasticSearchController.CheckUserProfileExistTask checkUserProfileExistTask = new ElasticSearchController.CheckUserProfileExistTask();
+
+        checkUserProfileExistTask.execute(userEditText.getText().toString());
+
+
+        UserProfile user;
+        try {
+            ElasticSearchController.GetUserProfileTask getUserProfileTask = new ElasticSearchController.GetUserProfileTask();
+            user = getUserProfileTask.get();
+            //UserHolder.getInstance().setUser(user);
+            // intent to MainActivity
+            // http://stackoverflow.com/questions/4878159/whats-the-best-way-to-share-data-between-activities
+            // authored by Cristian
+            // Accessed November 4, 2016
+
+
+            if (user.getUserName().equals(userEditText.getText().toString())) {
+                Intent map = new Intent(LoginActivity.this, main_menu.class);
+                startActivity(map);
+            }
+
+
+        } catch (Exception e) {
+            // no user was found
+            Toast.makeText(LoginActivity.this, "Username not found!", Toast.LENGTH_SHORT).show();
+        }
+
 
 
     }
