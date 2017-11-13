b352f6da333f153ee82fb33e4b43b0a3c90db959
slark <610158299@qq.com>
2017-11-13 13:51:26 -0700

    main menu test

---
+++ Echoes/app/src/androidTest/java/com/example/cmput301f17t19/echoes/main_menuTest.java
@@ -0,0 +1,76 @@
+/*
+* Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+*/
+
+package com.example.cmput301f17t19.echoes;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by taijieyang on 2017/11/13.
+ */
+
+public class main_menuTest extends ActivityInstrumentationTestCase2 {
+    private Solo solo;
+
+    public main_menuTest() {
+        super(com.example.cmput301f17t19.echoes.main_menu.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+    public void testViewMyHabit() {
+        // test if the button works correctly
+        solo.assertCurrentActivity("Wrong Activity", main_menu.class);
+        solo.clickOnView(solo.getView(R.id.View_My_Habits));
+        solo.assertCurrentActivity("Wrong Activity", MyHabitsActivity.class);
+    }
+
+//    public void testFollowing() {
+//        // test if the button works correctly
+//        solo.assertCurrentActivity("Wrong Activity", main_menu.class);
+//        solo.clickOnView(solo.getView(R.id.following));
+//        solo.assertCurrentActivity("Wrong Activity", .class);
+//    }
+
+//
+//    public void testTODO() {
+//        // test if the button works correctly
+//        solo.assertCurrentActivity("Wrong Activity", main_menu.class);
+//        solo.clickOnView(solo.getView(R.id.TODO));
+//        solo.assertCurrentActivity("Wrong Activity", .class);
+//    }
+
+    public void testHabitHistory() {
+        // test if the button works correctly
+        solo.assertCurrentActivity("Wrong Activity", main_menu.class);
+        solo.clickOnView(solo.getView(R.id.habit_history));
+        solo.assertCurrentActivity("Wrong Activity", HabitHistoryActivity.class);
+    }
+
+//    public void testHabitEventMap() {
+//        // test if the button works correctly
+//        solo.assertCurrentActivity("Wrong Activity", main_menu.class);
+//        solo.clickOnView(solo.getView(R.id.habit_event_map));
+//        solo.assertCurrentActivity("Wrong Activity", .class);
+//    }
+
+    public void testLogout() {
+        // test if the button works correctly
+        solo.assertCurrentActivity("Wrong Activity", main_menu.class);
+        solo.clickOnView(solo.getView(R.id.logout));
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+    }
+
+
+}
+
