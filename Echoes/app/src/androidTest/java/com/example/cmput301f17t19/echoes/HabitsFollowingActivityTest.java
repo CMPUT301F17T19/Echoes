0d6f5fabe8cb02675af5c637ba0a0ff87a761ff1
AmyShanLu <slu1@ualberta.ca>
2017-11-26 15:50:34 -0700

    unit test for HabitsFollowingActivity class

---
+++ Echoes/app/src/androidTest/java/com/example/cmput301f17t19/echoes/HabitsFollowingActivityTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) Team cmput301f17t19, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta
+ */
+
+package com.example.cmput301f17t19.echoes;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.example.cmput301f17t19.echoes.Activities.HabitsFollowingActivity;
+import com.example.cmput301f17t19.echoes.Activities.LoginActivity;
+import com.example.cmput301f17t19.echoes.Activities.MainMenuActivity;
+import com.example.cmput301f17t19.echoes.Controllers.OfflineStorageController;
+import com.example.cmput301f17t19.echoes.Models.UserProfile;
+import com.robotium.solo.Solo;
+
+/**
+ * Created by shanlu on 2017-11-26.
+ */
+
+public class HabitsFollowingActivityTest extends ActivityInstrumentationTestCase2 {
+    private Solo solo;
+
+    public HabitsFollowingActivityTest() {
+        super(LoginActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+    private void login() {
+        solo.assertCurrentActivity("Wrong Activity",  LoginActivity.class);
+        //create a new username login for testing
+        UserProfile userProfile = new UserProfile("dummy3");
+        OfflineStorageController offlineStorageController = new OfflineStorageController(getActivity().getApplicationContext(), userProfile.getUserName());
+        offlineStorageController.saveInFile(userProfile);
+
+
+        //enter the usrname "dummy3"
+        solo.enterText((EditText) solo.getView(R.id.username),"dummy3");
+        solo.clickOnView(solo.getView(R.id.username_sign_in_button));
+        solo.assertCurrentActivity("Wrong Activity", MainMenuActivity.class);
+    }
+
+    public void testHabitsFollowingActivity() {
+        login();
+
+        solo.assertCurrentActivity("Wrong Activity", MainMenuActivity.class);
+        solo.clickOnView(solo.getView(R.id.following));
+        solo.waitForActivity(HabitsFollowingActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", HabitsFollowingActivity.class);
+    }
+}
