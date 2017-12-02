310d431cbc45cb6ede33207d8a21ad8698b2b6c6
zhaozhen@ualberta.ca <zhaozhen@ualberta.ca>
2017-12-01 23:35:33 -0700

    adjustment

--- Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/Activities/UserMessageActivity.java
+++ Echoes/app/src/main/java/com/example/cmput301f17t19/echoes/Activities/UserMessageActivity.java
@@ -5,13 +5,17 @@
 package com.example.cmput301f17t19.echoes.Activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -38,6 +42,22 @@ public class UserMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.redPink));
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_message);
         this.setTitle(R.string.userMessage);
